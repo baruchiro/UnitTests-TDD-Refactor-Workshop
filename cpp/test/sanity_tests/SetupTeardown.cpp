b4d6a8dddb577f8268de89ec75559d0f614402a0
yakik <yaki.koren@gmail.com>
2020-03-24 07:19:15 +0200

    setup/teardown

---
+++ cpp/test/sanity_tests/SetupTeardown.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+namespace {
+  class MyTest:public::testing::Test {
+    public : int myNumber=0;
+
+    MyTest() {
+    }
+
+    virtual void SetUp() {
+      myNumber = 5;
+    }
+
+    virtual void TearDown() {
+      
+    }
+  };
+
+    TEST_F(MyTest, Test1) {
+        
+        myNumber+=5;
+        
+        EXPECT_EQ(10,myNumber);
+    }
+
+    TEST_F(MyTest, Test2) {
+        myNumber+=3;
+        
+        EXPECT_EQ(8,myNumber);
+    }
+}
