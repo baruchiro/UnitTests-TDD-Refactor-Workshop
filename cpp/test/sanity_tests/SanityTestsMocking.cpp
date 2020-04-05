1090ea7f34ed7b2eac106ec5dcb5bac8938f75f1
yakik <yaki.koren@gmail.com>
2020-04-05 17:15:16 +0300

    mocking clearer

--- cpp/test/sanity_tests/SanityTestsMocking.cpp
+++ cpp/test/sanity_tests/SanityTestsMocking.cpp
@@ -1,13 +1,36 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-class Turtle {
+/*class Turtle {
     virtual int PenDown() = 0;
 };
 
 class MockTurtle : public Turtle {
 public:
     MOCK_METHOD(int, PenDown, (), (override));
+};*/
+
+class Turtle {
+  public:;  
+  virtual ~Turtle() {};
+  virtual void PenUp() = 0;
+  virtual void PenDown() = 0;
+  virtual void Forward(int distance) = 0;
+  virtual void Turn(int degrees) = 0;
+  virtual void GoTo(int x, int y) = 0;
+  virtual int GetX() const = 0;
+  virtual int GetY() const = 0;
+};
+
+class MockTurtle : public Turtle {
+ public:
+  MOCK_METHOD(void, PenUp, (), (override));
+  MOCK_METHOD(void, PenDown, (), (override));
+  MOCK_METHOD(void, Forward, (int distance), (override));
+  MOCK_METHOD(void, Turn, (int degrees), (override));
+  MOCK_METHOD(void, GoTo, (int x, int y), (override));
+  MOCK_METHOD(int, GetX, (), (const, override));
+  MOCK_METHOD(int, GetY, (), (const, override));
 };
 
 using testing::AtLeast;
@@ -15,9 +38,11 @@ using testing::Return;// #1
 
 TEST(SanityTest, Mocking) {
     MockTurtle turtle;                             
-    EXPECT_CALL(turtle, PenDown())                  
-            .Times(AtLeast(1)).WillOnce(Return(100));
-int yaki = turtle.PenDown();
-    EXPECT_EQ(100,yaki);
+    EXPECT_CALL(turtle, GetX())                  
+            .Times(AtLeast(1)).WillOnce(Return(100)).WillOnce(Return(200));
+ int yaki = turtle.GetX();
+  EXPECT_EQ(100,yaki);
+  yaki = turtle.GetX();
+    EXPECT_EQ(200,yaki);
 
 }
--- cpp/test/sanity_tests/SetupTeardown.cpp
+++ cpp/test/sanity_tests/SetupTeardown.cpp
@@ -12,20 +12,16 @@ namespace {
     }
 
     virtual void TearDown() {
-      
     }
   };
 
     TEST_F(MyTest, Test1) {
-        
         myNumber+=5;
-        
         EXPECT_EQ(10,myNumber);
     }
 
     TEST_F(MyTest, Test2) {
         myNumber+=3;
-        
         EXPECT_EQ(8,myNumber);
     }
 }
