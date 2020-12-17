744dd08bff46bdb29516c99361e281725f41df20
Yaki Koren <yaki.koren@gmail.com>
2020-12-17 11:16:05 +0200

    cusomter account -> interface

--- Java/src/main/java/com/gildedrose/CustomerAccount.java
+++ Java/src/main/java/com/gildedrose/CustomerAccount.java
@@ -1,19 +1,7 @@
 package com.gildedrose;
 
-public class CustomerAccount {
-    private int serial;
-    
-    public CustomerAccount(int serial, int initialBalance) {
-      this.serial = serial;
-    }
-    
-    public void createOrder(CustomerOrder order) {
-      // ... }
-    
-    public int getOrderStatus() {
-      int result=0;
-      // ...
-      return result;
-    }
+public interface CustomerAccount {
+    public void createOrder(CustomerOrder order);
+    public int getOrderStatus();
   }
   
