dc7e12d4348c46a97637891468dcec1be99c37df
Yaki Koren <yaki.koren@gmail.com>
2020-12-17 07:25:15 +0200

    customeraccount for decorator pattern

---
+++ Java/src/main/java/com/gildedrose/CustomerAccount.java
@@ -0,0 +1,19 @@
+package com.gildedrose;
+
+public class CustomerAccount {
+    private int serial;
+    
+    public CustomerAccount(int serial, int initialBalance) {
+      this.serial = serial;
+    }
+    
+    public void createOrder(/*...*/) {
+      // ... }
+    
+    public int getOrderStatus() {
+      int result=0;
+      // ...
+      return result;
+    }
+  }
+  
