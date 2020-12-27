2fb50f12843e49c00bfbcf94f0321c76ad33f726
yaki koren <yaki@agilesparks.com>
2020-12-27 16:49:04 +0200

    approval tests examples

---
+++ csharp/GildedRose/EGR-Tests/legacycodeplaygroundtests/ApprovalTestsExamples.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalTests.Reporters;
+using ApprovalTests;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace EGR_Tests.legacycodeplaygroundtests
+{
+
+    public class ForApprovalDemonstration
+    {
+        public int first = 3;
+        public List<String> myList = new List<String> { "first", "second", "third" };
+        public override string ToString()
+        {
+            
+            return "For Approval: first=" + first.ToString() + "\nmyList="   + "\n" +  string.Join("\n", myList.ToArray())    ;
+        }
+    }
+
+
+    [TestClass]
+    public class ApprovalTestsExamples
+    {
+        [ApprovalTests.Namers.UseApprovalSubdirectory("Approvals")]
+        [UseReporter(typeof(DiffReporter))]
+        [TestMethod]
+        public void SimpleVerify()
+        {
+            int myNumber = 8;
+            Approvals.Verify(myNumber);
+        }
+
+        [ApprovalTests.Namers.UseApprovalSubdirectory("Approvals")]
+        [UseReporter(typeof(DiffReporter))]
+        [TestMethod]
+        public void ComplexVerify()
+        {
+            ForApprovalDemonstration complexClass = new ForApprovalDemonstration();
+            Approvals.Verify(complexClass);
+         }
+    }
+}
+
