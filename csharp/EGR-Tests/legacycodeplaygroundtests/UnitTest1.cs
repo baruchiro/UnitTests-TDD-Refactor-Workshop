582324840dad5a2f88579a36da421d93bf366734
yaki koren <yaki@agilesparks.com>
2020-12-27 10:10:22 +0200

    mrge from windows

---
+++ csharp/EGR-Tests/legacycodeplaygroundtests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalTests;
+using System;
+
+namespace EGR_Tests.legacycodeplaygroundtests
+{
+    //[UseReporter(typeof(VisualStudioReporter))]
+    //[TestFixture]
+    [TestClass]
+    public class ApprovalTestsExample
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Approvals.VerifyAll();
+        }
+    }
+}
